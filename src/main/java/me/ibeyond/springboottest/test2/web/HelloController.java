048f47e65b90a3ccf2171ec6bdd4fca24de21278
uptoser <450052550@qq.com>
2021-06-08 16:03:06 +0800

    增加了 模板

--- src/main/java/me/ibeyond/springboottest/test2/web/HelloController.java
+++ src/main/java/me/ibeyond/springboottest/test2/web/HelloController.java
@@ -1,24 +1,47 @@
 package me.ibeyond.springboottest.test2.web;
 
-import me.ibeyond.springboottest.config.UserSettings;
-import me.ibeyond.springboottest.test2.model.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import me.ibeyond.springboottest.test2.model.User;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 
-@RestController
+@Controller
 public class HelloController {
 
-    @Autowired
-//    private DiscoveryClient
-
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public void main() {
-        Test test = new Test();
-        test.setId("11");
-        test.setName("aaa");
-//        return user1;
+    @RequestMapping(value = "/success", method = RequestMethod.GET)
+    public String success(Map<String, Object> map) {
+        map.put("name","Terry Roy");
+        return "success";
     }
+    @RequestMapping(value = "/header", method = RequestMethod.GET)
+    public String header(Map<String, Object> map) {
+        return "header";
+    }
+    @RequestMapping("/study")
+    public String study(Map<String, Object> map, HttpServletRequest request) {
+        List<User> userList = new ArrayList<>();
+        userList.add(new User("小梦", 1));
+        userList.add(new User("小李", 2));
+        userList.add(new User("小张", 1));
+
+        map.put("userList", userList);
+
+        //1 女， 2 男
+        map.put("sex", 2);//
+        map.put("man", 2);//
+
+        map.put("desc", "欢迎来到<h1>梦学谷</h1>");
+
+        //将user对象绑定到session中
+        request.getSession().setAttribute("user", new User("小刘", 2));
+
+        return "study";
+    }
+
 }
